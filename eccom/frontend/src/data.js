27a69ef308b05452214c755cb292ff2afbc1ab5c
yadavabhi02 <121677144+yadavabhi02@users.noreply.github.com>
2023-04-26 11:23:51 +0530

    Create data.js

---
+++ eccom/frontend/src/data.js
@@ -0,0 +1,87 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://www.kindpng.com/picc/m/4-40720_transparent-fashion-models-png-fashion-man-jeans-png.png",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://www.kindpng.com/picc/m/63-633936_transparent-women-fashion-png-png-download.png",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://www.kindpng.com/picc/m/118-1180056_chic-stylish-fashion-women-jeans-png-transparent-png.png",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "SHIRT STYLE!",
+      cat: "women",
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "LOUNGEWEAR LOVE",
+      cat: "coats",
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      title: "LIGHT JACKETS",
+      cat: "jeans",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:2,
+      img:"https://www.kindpng.com/picc/m/39-398558_blue-tshirt-png-transparent-png.png",
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:4,
+      img:"https://www.kindpng.com/picc/m/491-4916552_designer-indian-anarkali-dresses-hd-png-download.png",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+      id:7,
+      img:"https://www.kindpng.com/picc/m/383-3834299_thumb-image-gowns-in-ball-transparent-background-hd.png",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+    {
+      id:9,
+      img:"https://www.kindpng.com/picc/m/390-3908758_leather-jacket-ladies-png-image-background-buse-lady.png",
+    },
+    {
+      id:10,
+      img:"https://www.kindpng.com/picc/m/189-1897938_high-heel-shoes-louis-vuitton-silver-heels-hd.png",
+    },
+  ]
